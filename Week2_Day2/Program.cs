f7e03b4797b94d4e0e6d620e4657d6b170619228
alexandriaware <ali.mercer92@gmail.com>
2017-10-03 14:15:18 -0400

    using arrays with loops

--- Week2_Day2/Program.cs
+++ Week2_Day2/Program.cs
@@ -17,9 +17,38 @@ namespace Week2_Day2
             //    Console.WriteLine(i);
             //}
 
-            for (int counter = 50; counter <= 100; counter++)
+            //for (int counter = 50; counter <= 100; counter++)
+            //{
+            //    Console.Write(counter);
+            //}
+
+            //string greeting = "My name is Little Bill";
+            //string[] words = greeting.Split();
+
+            //for(int i = 0; i < words.Length; i++)
+            //{
+            //    Console.WriteLine(words[i]);
+            //}
+
+
+            //Do it!
+            string weekdays = "Monday Tuesday Wednesday Thursday";
+            string[] days = weekdays.Split();
+
+            for(int i = 0; i < days.Length; i++)
             {
-                Console.WriteLine(counter);
+                Console.WriteLine(days[i]);
+            }
+
+
+            string beginning = "Once upon a time";
+            string[] storyWords = beginning.Split();
+
+            Array.Reverse(storyWords);
+
+            for(int i = 0; i < storyWords.Length; i++)
+            {
+                Console.WriteLine(storyWords[i]);
             }
 
 
